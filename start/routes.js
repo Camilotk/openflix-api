f38373ba66df55f3af428171e257cef721ba8c5f
Camilo Cunha de Azevedo <Camilotk@gmail.com>
2021-02-18 07:56:34 -0300

    :truck: Fixed the routes names
    Changed the wrong method calls in the routes file for the right ones

--- start/routes.js
+++ start/routes.js
@@ -27,7 +27,7 @@ Route.group(() => {
   Route.get('/:id', 'UserController.show').middleware('auth')
   Route.post('/', 'UserController.store').validator('CreateUser')
   Route.put('/:id', 'UserController.update').validator('UpdateUser').middleware('auth')
-  Route.delete('/:id', 'UserController.delete').middleware('auth')
+  Route.delete('/:id', 'UserController.destroy').middleware('auth')
 }).prefix('users')
 
 Route.group(() => {
@@ -35,46 +35,46 @@ Route.group(() => {
   Route.get('/:id', 'ActorController.show')
   Route.post('/', 'ActorController.store').validator('CreateActor')
   Route.put('/:id', 'ActorController.update').validator('UpdateActor')
-  Route.delete('/:id', 'ActorController.delete')
+  Route.delete('/:id', 'ActorController.destroy')
 }).prefix('actors').middleware(['auth', 'is:administrator'])
 
 Route.group(() => {
   Route.get('/', 'DirectorController.index')
   Route.get('/:id', 'DirectorController.show')
-  Route.post('/', 'DirectorController.create').validator('CreateDirector')
+  Route.post('/', 'DirectorController.store').validator('CreateDirector')
   Route.put('/:id', 'DirectorController.update').validator('UpdateDirector')
-  Route.delete('/:id', 'DirectorController.delete')
+  Route.delete('/:id', 'DirectorController.destroy')
 }).prefix('directors').middleware(['auth', 'is:administrator'])
 
 Route.group(() => {
   Route.get('/', 'GenreController.index')
   Route.get('/:id', 'GenreController.show')
-  Route.post('/', 'GenreController.create').validator('CreateGenre')
+  Route.post('/', 'GenreController.store').validator('CreateGenre')
   Route.put('/:id', 'GenreController.update').validator('UpdateGenre')
-  Route.delete('/:id', 'GenreController.delete')
+  Route.delete('/:id', 'GenreController.destroy')
 }).prefix('genres').middleware(['auth', 'is:administrator'])
 
 Route.group(() => {
   Route.get('/', 'TagController.index')
   Route.get('/:id', 'TagController.show')
-  Route.post('/', 'TagController.create').validator('CreateTag')
+  Route.post('/', 'TagController.store').validator('CreateTag')
   Route.put('/:id', 'TagController.update').validator('UpdateTag')
-  Route.delete('/:id', 'TagController.delete')
+  Route.delete('/:id', 'TagController.destroy')
   Route.get('/:id/videos', 'TagController.videos')
 }).prefix('tags').middleware(['auth', 'is:administrator'])
 
 Route.group(() => {
   Route.get('/', 'VideoController.index')
   Route.get('/:id', 'VideoController.show')
-  Route.post('/', 'VideoController.create').validator('CreateVideo')
+  Route.post('/', 'VideoController.store').validator('CreateVideo')
   Route.put('/:id', 'VideoController.update').validator('UpdateVideo')
-  Route.delete('/:id', 'VideoController.delete')
+  Route.delete('/:id', 'VideoController.destroy')
 }).prefix('video').middleware(['auth', 'is:administrator'])
 
 Route.group(() => {
   Route.get('/', 'VideoLinkController.index')
   Route.get('/:id', 'VideoLinkController.show')
-  Route.post('/', 'VideoLinkController.create').validator('CreateLink')
+  Route.post('/', 'VideoLinkController.store').validator('CreateLink')
   Route.put('/:id', 'VideoLinkController.update').validator('UpdateLink')
-  Route.delete('/:id', 'VideoLinkController.delete')
+  Route.delete('/:id', 'VideoLinkController.destroy')
 }).prefix('links').middleware(['auth', 'is:administrator'])
